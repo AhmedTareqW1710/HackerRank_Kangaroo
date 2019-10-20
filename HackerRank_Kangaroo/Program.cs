3cc9345cee5d4642361f2e2513bb78e06d5301c4
aebrahim <aebrahim@THQ1LTAEBRAHIM>
2019-10-20 13:00:33 +0300

    100 % of test cases passed

--- HackerRank_Kangaroo/Program.cs
+++ HackerRank_Kangaroo/Program.cs
@@ -1,15 +1,60 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
 using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace HackerRank_Kangaroo
+class Solution
 {
-    class Program
+
+    // Complete the kangaroo function below.
+    static string kangaroo(int x1, int v1, int x2, int v2)
     {
-        static void Main(string[] args)
+        
+        if ((x2 > x1  && v2 >= v1)  || (x2 < x1 && v2 <= v1)) 
         {
+            return "NO";
         }
+
+
+        if (x1 < x2)
+        {
+            if ((x2 - x1) % (v1 - v2) == 0)
+            {
+                return "YES";
+            }
+        }
+        
+
+        return "NO";
+    }
+
+    static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] x1V1X2V2 = Console.ReadLine().Split(' ');
+
+        int x1 = Convert.ToInt32(x1V1X2V2[0]);
+
+        int v1 = Convert.ToInt32(x1V1X2V2[1]);
+
+        int x2 = Convert.ToInt32(x1V1X2V2[2]);
+
+        int v2 = Convert.ToInt32(x1V1X2V2[3]);
+
+        string result = kangaroo(x1, v1, x2, v2);
+
+        Console.WriteLine(result);
+        Console.ReadKey();
+        
     }
 }
